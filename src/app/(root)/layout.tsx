a226b09417c478668d99e41f0478ed25b6ff74d4
takemasa32 <takemasa0302@gmail.com>
2024-06-17 11:37:58 +0900

    Iconの設定

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -24,6 +24,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
+        {/* ICON */}
+        <link rel="icon" href="./../favicon.ico" />
       </head>
       <body>
         <div

4be344ee9e934aeaef12259324ae66f340cc510d
takemasa32 <takemasa0302@gmail.com>
2024-12-08 06:36:52 +0900

    2023年の情報を追加 その他修正

---
+++ src/app/(root)/events/2023teikiensoukai/data.ts
@@ -0,0 +1,60 @@
+//2023年
+export type BandData = {
+  name: string;
+  copyFrom: string;
+  comment: string;
+  photo: string;
+  order: number;
+};
+export const bandData = [
+  {
+    name: "ポルカめっちゃ好きングレイ",
+    copyFrom: "ポルカドットスティングレイ",
+    comment: "",
+    photo: "/image/2023teien/01.jpg",
+    order: 1,
+  },
+  {
+    name: "WOOPER LOOPER",
+    copyFrom: "SUPER BEAVER",
+    comment: "",
+    photo: "/image/2023teien/02.jpg",
+    order: 2,
+  },
+  {
+    name: "ホントつら、日々",
+    copyFrom: "ポルノグラフィティ",
+    comment: "",
+    photo: "/image/2023teien/03.jpg",
+    order: 3,
+  },
+  {
+    name: "錯乱前立腺",
+    copyFrom: "錯乱前戦",
+    comment: "",
+    photo: "/image/2023teien/04.jpg",
+    order: 4,
+  },
+  {
+    name: "マアイ feat.チンマンホウ",
+    copyFrom: "レキシ",
+    comment: "",
+    photo: "/image/2023teien/05.jpg",
+    order: 5,
+  },
+
+  {
+    name: "0.01mm sagami original",
+    copyFrom: "9mm parabellum bullet",
+    comment: "",
+    photo: "/image/2023teien/06.jpg",
+    order: 6,
+  },
+  {
+    name: "ケイシュンは真っ黒",
+    copyFrom: "カラスは真っ白",
+    comment: "",
+    photo: "/image/2023teien/07.jpg",
+    order: 7,
+  },
+];

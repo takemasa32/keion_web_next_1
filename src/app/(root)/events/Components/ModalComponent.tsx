590f3e6bd5fe82563b7795fab624e98fa804c673
takemasa32 <takemasa0302@gmail.com>
2024-12-10 07:52:47 +0900

    表示を修正

--- src/app/(root)/events/Components/ModalComponent.tsx
+++ src/app/(root)/events/Components/ModalComponent.tsx
@@ -111,7 +111,7 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
       isOpen={!!selectedBand}
       onRequestClose={closeModal}
       contentLabel="Band Details"
-      className="fixed mx-4 my-6 inset-0 flex items-center justify-center z-50 transition-opacity duration-600"
+      className="fixed mx-3 my-6 inset-0 flex items-center justify-center z-50 transition-opacity duration-600"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity duration-600"
       ariaHideApp={false}
     >
@@ -143,7 +143,7 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
         </div>
         <h2 className="text-2xl text-gray-700 text-center font-bold">{selectedBand?.name}</h2>
         <p className="text-sm text-gray-500 text-center font-bold mb-2">{selectedBand?.copyFrom}</p>
-        <div className="overflow-y-auto max-h-30 border-t border-b border-gray-300 my-4 p-4 bg-gray-100 rounded-lg shadow-inner">
+        <div className="overflow-y-auto h-28 border-t border-b border-gray-300 my-4 p-4 bg-gray-100 rounded-lg shadow-inner">
           <p
             className="text-gray-700 text-center leading-relaxed whitespace-pre-wrap"
             dangerouslySetInnerHTML={{
@@ -165,7 +165,7 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
         <div className="flex items-center justify-center mb-4">
           <button
             onClick={closeModal}
-            className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600"
           >
             close
           </button>
